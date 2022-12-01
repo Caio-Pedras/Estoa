8ea63052bdbd5fc0593db072a2ed1e7c64de36e0
CaioPedras <caio.vp@gmail.com>
2022-12-01 19:06:52 -0300

    feat: app config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
+import errorHandler from "./middlewares/errorHandler";
+import router from "./routes/index";
 
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(router);
+app.use(errorHandler);
 
 export default app;
