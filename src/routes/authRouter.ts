e43284fe588afff44af1c6ee2e2028264d456ba1
CaioPedras <caio.vp@gmail.com>
2022-12-01 19:05:13 -0300

    chore: create endpoints

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,28 @@
 import { Router } from "express";
+import {
+  deleteUserById,
+  getUserById,
+  postLogin,
+  postNewUser,
+  updateUserInfo,
+} from "../controllers/authController";
+import { schemaValidator } from "../middlewares/schemaValidator";
+import { tokenValidator } from "../middlewares/tokenValidator";
+import authSchema from "../schemas/authSchema";
+import loginSchema from "../schemas/loginSchema";
+import updateAuthSchema from "../schemas/updateAuthSchema";
 
 const authRouter = Router();
 
-authRouter.post("/users");
-authRouter.get("/users/:id");
+authRouter.post("/users", schemaValidator(authSchema), postNewUser);
+authRouter.post("/users/login", schemaValidator(loginSchema), postLogin);
+authRouter.get("/users/:id", getUserById);
+authRouter.put(
+  "/users/:id",
+  schemaValidator(updateAuthSchema),
+  tokenValidator,
+  updateUserInfo
+);
+authRouter.delete("/users/:id", tokenValidator, deleteUserById);
 
 export default authRouter;
